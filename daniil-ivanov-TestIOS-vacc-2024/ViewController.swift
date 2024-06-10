f68e840e3342ef860680bbfb436abec325002c20
Daniil <developer_ivanov.d.a@icloud.com>
2024-06-10 19:39:57 +0300

    Initial Commit

---
+++ daniil-ivanov-TestIOS-vacc-2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  daniil-ivanov-TestIOS-vacc-2024
+//
+//  Created by Даниил Иванов on 10.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
